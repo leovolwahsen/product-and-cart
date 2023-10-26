70c675f9c64954f8fa371696af28ee16c9012f2e
Leo <volwahsenl@gmail.com>
2023-10-26 11:36:13 +0200

    update views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import ProductView from '../views/ProductView.vue'
 
 const routes = [
   {
@@ -9,9 +9,9 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutView
+    path: '/products',
+    name: 'products',
+    component: ProductView
   }
 ]
 
